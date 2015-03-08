242e64633d2fa2ab0b196f157b20d3d0f58605dc
Nathaniel Foster <nathanielsmusic@gmail.com>
2015-03-08 14:57:47 -0400

    updated camera position to be in center of tweets, tweets now face camera

--- js/geotwitter.js
+++ js/geotwitter.js
@@ -3,111 +3,95 @@
 
   window.onload = function() {
     var tweetElement,
-    tweetObject,
-    tweet;
+      tweetObject, tweet;
 
     if (typeof data !== undefined) {
       camera = new THREE.PerspectiveCamera(
         40, window.innerWidth / window.innerHeight, 1, 10000 );
 
-        camera.position.y = 500;
-        camera.position.z = 3000;
+      deviceControls = new THREE.DeviceOrientationControls( camera );
 
-        deviceControls = new THREE.DeviceOrientationControls( camera );
+      scene = new THREE.Scene();
 
-        scene = new THREE.Scene();
-
-        var ELEM_WIDTH = 200,
+      var ELEM_WIDTH = 200,
         ELEM_HEIGHT = 100;
 
-        for (var i = 0; i < data.length; i++) {
-          tweet = data[i];
+      var isFullscreen = false;
 
-          tweetElement = document.createElement( 'div' );
-          tweetElement.className = 'tweet';
-          tweetElement.style.width = ELEM_WIDTH + 'px';
-          tweetElement.style.height = ELEM_HEIGHT + 'px';
-          tweetElement.innerHTML = tweet.text;
+      for (var i = 0; i < data.length; i++) {
+        tweet = data[i];
 
-          tweetObject = new THREE.CSS3DObject( tweetElement );
-          tweetObject.position.x = tweet.lat * 10000 * 50;
-          // y could be determined by age of tweet
-          tweetObject.position.y = Math.random() * 1000;
-          tweetObject.position.z = tweet.lon * 10000 * 50;
+        tweetElement = document.createElement( 'div' );
+        tweetElement.className = 'tweet';
+        tweetElement.style.width = ELEM_WIDTH + 'px';
+        tweetElement.style.height = ELEM_HEIGHT + 'px';
+        tweetElement.innerHTML = tweet.text;
 
-          scene.add( tweetObject );
+        tweetObject = new THREE.CSS3DObject( tweetElement );
+        tweetObject.position.x = tweet.lat * 100000 * 50;
+
+        // y could be determined by age of tweet
+        tweetObject.position.y = Math.random() * 1000;
+        tweetObject.position.z = tweet.lon * 100000 * 50;
+
+        tweetObject.lookAt( camera.position );
+
+        scene.add( tweetObject );
+      }
+
+      renderer = new THREE.CSS3DStereoRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      renderer.domElement.style.position = 'absolute';
+      document.getElementById( 'container' )
+        .appendChild( renderer.domElement );
+
+      window.addEventListener( 'resize', onWindowResize, false );
+
+      window.addEventListener( 'touchend', function () {
+
+        if ( isFullscreen === false ) {
+
+          document.body.webkitRequestFullscreen();
+
+          isFullscreen = true;
+
+        } else {
+
+          document.webkitExitFullscreen();
+
+          isFullscreen = false;
+
         }
 
-        renderer = new THREE.CSS3DStereoRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.domElement.style.position = 'absolute';
-        document.getElementById( 'container' ).appendChild( renderer.domElement );
+      });
 
-        controls = new THREE.TrackballControls( camera, renderer.domElement );
-        controls.rotateSpeed = 0.5;
-        controls.minDistance = 100;
-        controls.maxDistance = 6000;
-        controls.addEventListener( 'change', render );
-
-
-
-        window.addEventListener( 'resize', onWindowResize, false );
-
-
-        var isFullscreen = false;
-
-        window.addEventListener( 'touchend', function () {
-
-          if ( isFullscreen === false ) {
-
-            document.body.webkitRequestFullscreen();
-
-            isFullscreen = true;
-
-          } else {
-
-            document.webkitExitFullscreen();
-
-            isFullscreen = false;
-
-          }
-
-        } );
-
-        animate();
-
-      }
-
-
-
-      function animate() {
-        // console.log("animate controls");
-        requestAnimationFrame( animate );
-
-        controls.update();
-
-        deviceControls.update();
-
-        render();
-      }
-
-      function render() {
-        // console.log("render");
-        renderer.render( scene, camera );
-      }
-
-      function onWindowResize() {
-
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize( window.innerWidth, window.innerHeight );
-
-        render();
-
-      }
+      animate();
 
     }
 
+    function animate() {
+      requestAnimationFrame( animate );
 
-  })(tweetList);
+      deviceControls.update();
+
+      render();
+    }
+
+    function render() {
+      renderer.render( scene, camera );
+    }
+
+    function onWindowResize() {
+
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize( window.innerWidth, window.innerHeight );
+
+      render();
+
+    }
+
+  }
+
+})(tweetList);
